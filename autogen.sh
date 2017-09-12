0778463a6166e0ab7bf16ca47607fb722664ec2b
Alec Ari <alec@onelabs.com>
2017-09-12 00:41:39 -0500

    A couple script fixes and clean-ups.
    Command parsing and module-dir wasn't right.
    
    Signed-off-by: Alec Ari <alec@onelabs.com>

--- autogen.sh
+++ autogen.sh
@@ -2,13 +2,11 @@
 
 autoreconf -v --install || \
 	{
-	echo -e "Error running autoreconf.\n" ;
-	exit 1 ;
+		echo -e "Error running autoreconf.\n" ; exit 1 ;
 	}
 
 # Do a temporary configure to generate initial makefiles
 ./configure --with-linux-dir="/lib/modules/$(uname -r)/build" "$@" || \
 	{
-	echo -e "Error running configure.\n" ;
-	exit 1 ;
+		echo -e "Error running configure.\n" ;	exit 1 ;
 	}
