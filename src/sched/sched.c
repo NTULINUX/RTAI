3a86acca971bd3738f2b93702131dd3d528ce176
Alec Ari <neotheuser@ymail.com>
2020-05-08 21:18:11 -0500

    Revert "A possible fix for #13 ?"
    This reverts commit e5b522c36ffb21d91b3169269a7b3599082650f4.
    
    This breaks LinuxCNC's latency-test and RTAI testsuite
    
    Fixes issue #13 though but not properly.

--- src/sched/sched.c
+++ src/sched/sched.c
@@ -2612,7 +2612,7 @@ static void __rtai_lxrt_exit(void)
 
 	rtai_tskext(kthread_server_thread, TSKEXT3) = (void *)1;
 	rt_task_resume(rtai_tskext_t(kthread_server_thread, TSKEXT0));
-	while (rtai_tskext(kthread_server_thread, TSKEXT3)) msleep(50);
+	while (rtai_tskext(kthread_server_thread, TSKEXT3)) msleep(5);
 
 	lxrt_killall();
 
